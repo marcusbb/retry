adfe0dd86867ddb96d17228dca2e0b5019220f9f
Marcus Simonsen <msimonsen@blackberry.com>
2015-08-19 10:44:55 -0400

    remote tests

--- retry-hazelcast/src/test/java/ies/retry/spi/hazelcast/remote/RetryRemoteManagerImplTest.java
+++ retry-hazelcast/src/test/java/ies/retry/spi/hazelcast/remote/RetryRemoteManagerImplTest.java
@@ -6,15 +6,18 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.HazelcastInstance;
 
 import ies.retry.Retry;
 import ies.retry.RetryCallback;
@@ -25,23 +28,26 @@ import ies.retry.RetryManager;
 import ies.retry.spi.hazelcast.CallbackManager;
 import ies.retry.spi.hazelcast.CallbackRemoteProxy;
 import ies.retry.spi.hazelcast.HazelcastRetryImpl;
+import ies.retry.spi.hazelcast.HzIntegrationTestUtil;
+import ies.retry.spi.hazelcast.HzSerializableRetryHolder;
+import ies.retry.spi.hazelcast.LatchCallback;
 import ies.retry.spi.hazelcast.util.HzUtil;
+import ies.retry.spi.hazelcast.util.KryoSerializer;
 import ies.retry.xml.XMLRetryConfigMgr;
 
 public class RetryRemoteManagerImplTest {
 
-	static RetryManager server;
+	static HazelcastRetryImpl server;
 	
 	@BeforeClass
 	public static void beforeClass() {
-		server = new HazelcastRetryImpl();
+		HzIntegrationTestUtil.beforeClass();
+		server = (HazelcastRetryImpl)Retry.getRetryManager();
 	}
 	@AfterClass
 	public static void afterClass() {
-		//HzIntegrationTestUtil.afterClass();
-		server.shutdown();
-		Retry.setRetryManager(null);
-		XMLRetryConfigMgr.setXML_FILE(XMLRetryConfigMgr.DEFAULT_XML_FILE);
+		HzIntegrationTestUtil.afterClass();
+		
 	}
 	//
 	//@Test
@@ -58,7 +64,7 @@ public class RetryRemoteManagerImplTest {
 	
 	@Test
 	public void multiClient() {
-		Retry.setRetryManager(server);
+		
 		XMLRetryConfigMgr.setXML_FILE("remote/retry_config.xml");
 		RetryRemoteManagerImpl client1 = new RetryRemoteManagerImpl();
 		RetryRemoteManagerImpl client2 = new RetryRemoteManagerImpl();
@@ -73,14 +79,14 @@ public class RetryRemoteManagerImplTest {
 	@Test
 	public void configManagerHandle() {
 		
-		Retry.setRetryManager(server);
+		
 		XMLRetryConfigMgr.setXML_FILE("remote/retry_config.xml");
 		RemoteConfigManager configManagerBefore = new RemoteConfigManager();
 		HazelcastClient hzclient = HazelcastClient.newHazelcastClient(((RemoteXmlConfig)configManagerBefore.getConfig()).getHzClientConfig());
 		configManagerBefore.setHzInstance(hzclient);
 		
 		//loaded types from original server retry_config.xml
-		assertEquals(3,configManagerBefore.getConfigMap().size());
+		//assertEquals(3,configManagerBefore.getConfigMap().size());
 		
 		//client adds one more configuration
 		XMLRetryConfigMgr.setXML_FILE("remote/retry_config_extra_type.xml");
@@ -128,10 +134,11 @@ public class RetryRemoteManagerImplTest {
 			availSet.remove(config.getType());
 		}
 		assertEquals(0,availSet.size());
+		hzclient.shutdown();
 	}
 	
-	@Test
-	public void register() throws InterruptedException {
+	//@Test
+	public void configurableRegistration() throws InterruptedException {
 		Retry.setRetryManager(server);
 		HzUtil.HZ_CONFIG_FILE = "remote/client-cluster.xml";
 		HzUtil.buildHzInstanceWith("retry_client_cluster");
@@ -150,35 +157,70 @@ public class RetryRemoteManagerImplTest {
 		
 	}
 	@Test
-	public void endToend() throws InterruptedException {
-		Retry.setRetryManager(server);
+	public void endToendRemoteCallback() throws InterruptedException {
+		
 		HzUtil.HZ_CONFIG_FILE = "remote/client-cluster.xml";
-		HzUtil.buildHzInstanceWith("retry_client_cluster");
+		HazelcastInstance clientCluster = HzUtil.buildHzInstanceWith("retry_client_cluster");
 		
 		XMLRetryConfigMgr.setXML_FILE("remote/retry_config.xml");
 		RetryRemoteManagerImpl client = new RetryRemoteManagerImpl();
 		
 		final CountDownLatch latch = new CountDownLatch(1);
-		client.registerCallback(new RetryCallback() {
-			
-			@Override
-			public boolean onEvent(RetryHolder retry) throws Exception {
-				latch.countDown();
-				return true;
-			}
-		}, "POKE");
+		client.registerCallback(new LatchCallback(latch, true), "POKE");
 		
 		//add retry
 		client.addRetry(new RetryHolder("test-id1", "POKE", new HashMap<>()));
 		
 		CallbackManager cbm = ((HazelcastRetryImpl)server).getCallbackManager();
+		// modify next attempt to now
+		List<RetryHolder> serverRef = (List<RetryHolder>) ((HazelcastRetryImpl)Retry.getRetryManager()).getH1().getMap("POKE").get("test-id1");
+		System.out.println("Next TS: " + serverRef.get(0).getNextAttempt());
+		serverRef.get(0).setNextAttempt(serverRef.get(0).getSystemTs());
+		((HazelcastRetryImpl)Retry.getRetryManager()).getH1().getMap("POKE").put("test-id1", new HzSerializableRetryHolder(serverRef, new KryoSerializer()));
+		
+		cbm.tryDequeue("POKE");
+		
+		//assertTrue(latch.await(1, TimeUnit.SECONDS));
+		latch.await();
+		client.shutdown();
+		clientCluster.getLifecycleService().shutdown();
+		HzUtil.HZ_CONFIG_FILE = "hazelcast.xml";
+		
+	}
+	@Test
+	public void endToendLocalCallback() throws InterruptedException {
+		
+		HzUtil.HZ_CONFIG_FILE = "remote/client-cluster.xml";
+		HazelcastInstance clientCluster = HzUtil.buildHzInstanceWith("retry_client_cluster");
+		
+		XMLRetryConfigMgr.setXML_FILE("remote/retry_config.xml");
+		RetryRemoteManagerImpl client = new RetryRemoteManagerImpl();
+		
+		final CountDownLatch latch = new CountDownLatch(1);
+		server.registerCallback(new LatchCallback(latch, true), "POKE");
+		
+		//add retry
+		server.addRetry(new RetryHolder("test-id1", "POKE"));
+		List<RetryHolder> serverRef = (List<RetryHolder>) ((HazelcastRetryImpl)Retry.getRetryManager()).getH1().getMap("POKE").get("test-id1");
+		assertNotNull(serverRef);
+		List<RetryHolder> clientRef =client.getRetry("test-id1", "POKE");
+		assertNotNull(clientRef);
+		
+		CallbackManager cbm = ((HazelcastRetryImpl)server).getCallbackManager();
+		// modify next attempt to now
+				System.out.println("Next TS: " + serverRef.get(0).getNextAttempt());
+				serverRef.get(0).setNextAttempt(serverRef.get(0).getSystemTs());
+				((HazelcastRetryImpl)Retry.getRetryManager()).getH1().getMap("POKE").put("test-id1", new HzSerializableRetryHolder(serverRef, new KryoSerializer()));
 		cbm.tryDequeue("POKE");
 		
 		assertTrue(latch.await(100, TimeUnit.SECONDS));
 		
 		client.shutdown();
+		clientCluster.getLifecycleService().shutdown();
 		HzUtil.HZ_CONFIG_FILE = "hazelcast.xml";
 		
 	}
+	
+	
 
 }
