34f9649987613c5d7efb75a77433ae5ddcedc529
Marcus Simonsen <msimonsen@blackberry.com>
2015-08-21 11:20:49 -0400

    clean up

--- retry-hazelcast/src/main/java/ies/retry/spi/hazelcast/util/RetryUtil.java
+++ retry-hazelcast/src/main/java/ies/retry/spi/hazelcast/util/RetryUtil.java
@@ -8,8 +8,6 @@ import ies.retry.spi.hazelcast.StateManager;
 import ies.retry.spi.hazelcast.StateManager.LoadingState;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
